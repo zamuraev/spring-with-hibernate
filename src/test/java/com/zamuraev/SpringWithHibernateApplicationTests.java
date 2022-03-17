87bbd959998d7039c77d5f5e0ce5469055d2374e
zamuraev <zamuraev.sergey@gmail.com>
2022-03-17 10:35:44 +0200

    Initial commit

---
+++ src/test/java/com/zamuraev/SpringWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamuraev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
